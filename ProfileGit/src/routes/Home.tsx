20e2297795e86e111e72a702610dc73b313758f7
WRSG-Goncalves <wilssp@gmail.com>
2023-03-31 11:57:39 -0300

    Commit inicial do teste
    Commit inicial do teste

---
+++ ProfileGit/src/routes/Home.tsx
@@ -0,0 +1,42 @@
+import { UserProps } from '../types/user'
+import Search from '../components/Search'
+import { useState } from 'react'
+import User from '../components/User'
+import Error from '../components/Error'
+
+const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null)
+  const [error, setError] = useState(false)
+
+  const loadUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
+    try {
+      const res = await fetch(`https://api.github.com/users/${userName}`)
+      const data = await res.json()
+      if (res.status === 404) {
+        setError(true)
+        return
+      }
+
+      const { avatar_url, login, location, followers, following } = data
+
+      const userData: UserProps = { avatar_url, login, location, followers, following }
+
+      setUser(userData)
+
+    } catch (error) {
+      console.log(`erro acessando o github : ${error}`)
+    }
+  }
+  return (
+    <div>
+      <Search loadUser={loadUser} />
+      {user && <User {...user} />}
+      {error && <Error />}
+
+    </div>
+  )
+}
+
+export default Home
